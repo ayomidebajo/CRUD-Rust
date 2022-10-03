caf7f22687733a2d8bfdd3db078e0fdbdcda66f6
Ayomide Bajo <oluwashinabajo@gmail.com>
2022-10-03 18:38:00 +0100

    add first request

--- crud-rust/src/main.rs
+++ crud-rust/src/main.rs
@@ -1,6 +1,11 @@
 use tide::prelude::*;
 use tide::{Request, Response};
 
+struct TestRequest {
+    name: String,
+    typ: String,
+}
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let port: &str = "0.0.0.0:8080";
@@ -8,4 +13,13 @@ async fn main() -> tide::Result<()> {
     println!("Server listening at port {:?}", &port);
     app.listen(port).await?;
     Ok(())
+}
+
+async fn make_test_request(mut req: Request<()>) -> tide::Result {
+    let TestRequest { name, typ } = req.body_json().await?;
+
+    Ok(format!(
+        "Hello this is a test request with a {} and a {}",
+        name, typ
+    ))
 }
