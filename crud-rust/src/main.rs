c00fbb07751fe050fb42de508f3f4fd4b229413d
Ayomide Bajo <oluwashinabajo@gmail.com>
2022-10-05 18:35:45 +0100

    add new commit

--- crud-rust/src/main.rs
+++ crud-rust/src/main.rs
@@ -1,16 +1,21 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use tide::prelude::*;
 use tide::{Request, Response};
 
+#[derive(Debug, Deserialize)]
 struct TestRequest {
     name: String,
     typ: String,
 }
 
+// write get request
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let port: &str = "0.0.0.0:8080";
-    let app = tide::new();
+    let port: &str = "127.0.0.1:8080";
+    let mut app = tide::new();
+    app.at("/orders/shoes").post(make_test_request);
     println!("Server listening at port {:?}", &port);
     app.listen(port).await?;
     Ok(())
@@ -18,12 +23,9 @@ async fn main() -> tide::Result<()> {
 
 // Post request
 async fn make_test_request(mut req: Request<()>) -> tide::Result {
-    let mut db = HashMap::new();
+    // let mut db = HashMap::<String, String>::new();
     let TestRequest { name, typ } = req.body_json().await?;
     // db.insert(name, typ);
-    println!("db {}", db);
-    Ok(format!(
-        "Hello this is a test request with a {} and a {}",
-        name, typ
-    ))
+    // println!("db {:?}", db);
+    Ok(format!("Hello this is a test request with a {} and a {}", name, typ).into())
 }
